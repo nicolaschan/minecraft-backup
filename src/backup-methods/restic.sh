3afbdc712f2a17e08a4cbdad2090b174c31067c4
Nicolas Chan <nicolas@nicolaschan.com>
2020-03-21 22:28:36 -0700

    Remove restic stats because it's slow

--- src/backup-methods/restic.sh
+++ src/backup-methods/restic.sh
@@ -18,9 +18,7 @@ minecraft-backup-backup () {
 minecraft-backup-check () {
   local WORLD_SIZE_BYTES
   WORLD_SIZE_BYTES=$(du -b --max-depth=0 "$SERVER_WORLD" | awk '{print $1}')
-  local RESTIC_SIZE
-  RESTIC_SIZE=$(restic stats -r "$RESTIC_REPO" | tail -n1 | awk -F' ' '{ print $3 " " $4 }')
-  echo "$WORLD_SIZE_BYTES/$RESTIC_SIZE"
+  echo "$WORLD_SIZE_BYTES"
 }
 
 minecraft-backup-epilog () {
